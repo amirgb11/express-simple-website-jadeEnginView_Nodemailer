46b9929843a48d0174950744d03493fb417ba04a
amrdehghan1377 <31191650+amrdehghan1377@users.noreply.github.com>
2018-09-27 11:50:40 +0330

    2. added views

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ var path = require('path');
 var nodemailer = require('nodemailer');
 var app = express();
 
+app.set('views' , path.join(__dirname , 'views'));
+app.set('view engine' , 'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
+app.use(express.static(path.join(__dirname , 'public')))
 
 app.get('/' , function(req , res){
-    res.send('Hello World');
+    res.render('index');
 })
+
+app.get('/about' , function(req , res){
+    res.render('about');
+})
+
+
+app.listen(3000);
+console.log("app is running on port 3000")
