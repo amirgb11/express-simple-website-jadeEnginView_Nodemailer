385bee489ec33825415a695d6dcb0eb1ec2beeed
amrdehghan1377 <31191650+amrdehghan1377@users.noreply.github.com>
2018-10-18 00:24:25 +0330

    3. add nodemailer

--- app.js
+++ app.js
@@ -11,12 +11,45 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
 app.use(express.static(path.join(__dirname , 'public')))
 
-app.get('/' , function(req , res){
+app.get('/' , (req , res) => {
     res.render('index');
 })
 
-app.get('/about' , function(req , res){
+app.get('/about' , (req , res) => {
     res.render('about');
+})
+
+app.get('/contact' , (req , res) => {
+    res.render('contact')
+})
+
+app.post('/contact/send' , (req, res) => {
+    var transporter = nodemailer.createTransport({
+        service : 'Gmail' , 
+        auth : {
+            user: 'amirdehghan1377@gmail.com' , 
+            pass: 'a+m+i+r77.'
+        }
+    });
+
+    var mailOptions = {
+        from: 'amir dehghan <amirdehghan1377@gmail.com>' , 
+        to: 'amirdehghan1377@gmail.com',
+        subject: 'Website Submissoin',
+        text: 'Submission with following details ... Name' +req.body.name+ 'Email' +req.body.email+ 'Message' +req.body.message,
+        html: '<p>Submission with following details ... </p><ul><li>Name : '+req.body.name+'</li><li>Email : '+req.body.email+'</li><li>Message : '+req.body.message+'</li></ul>'
+    };
+
+    transporter.sendMail(mailOptions , (err, info) => {
+        if (err) {
+            console.log(err);
+            res.redirect('/');
+        }
+        else{
+            console.log('Message sent : '+info.response)
+            res.redirect('/');
+        }
+    })
 })
 
 
